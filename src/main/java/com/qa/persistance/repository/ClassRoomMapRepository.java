61fddc3c9604c42050fab3a5ef8df391dbf65bee
jjbrooks251 <jjbrooks251@outlook.com>
2019-06-07 11:01:11 +0100

    classroom map repo added

---
+++ src/main/java/com/qa/persistance/repository/ClassRoomMapRepository.java
@@ -0,0 +1,37 @@
+package com.qa.persistance.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.qa.persistance.domain.Classroom;
+import com.qa.util.JSONUtil;
+
+public class ClassRoomMapRepository implements ClassroomRepository {
+
+	@Inject
+	JSONUtil util;
+
+	@PersistenceContext(unitName = "primary")
+	EntityManager manager;
+
+	private Map<Integer, Classroom> classroomMap = new HashMap<Integer, Classroom>();
+
+	public Map<Integer, Classroom> getClassroomMap() {
+		return classroomMap;
+	}
+
+	public void setClassroomMap(Map<Integer, Classroom> classroomMap) {
+		this.classroomMap = classroomMap;
+	}
+
+	@Override
+	public String getAllClassrooms() {
+
+		return classroomMap.toString();
+	}
+
+}
