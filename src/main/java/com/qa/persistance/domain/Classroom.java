f4cf043ddff40610f53c917be193fdbc674c19eb
jjbrooks251 <jjbrooks251@outlook.com>
2019-06-07 10:38:54 +0100

    packages and classroom entity added

---
+++ src/main/java/com/qa/persistance/domain/Classroom.java
@@ -0,0 +1,55 @@
+package com.qa.persistance.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Classroom {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column(length = 30)
+	private String trainerFirst;
+	@Column(length = 30)
+	private String trainerLast;
+
+	public Classroom() {
+		super();
+	}
+
+	public Classroom(long id, String trainerFirst, String trainerLast) {
+		super();
+		this.id = id;
+		this.trainerFirst = trainerFirst;
+		this.trainerLast = trainerLast;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTrainerFirst() {
+		return trainerFirst;
+	}
+
+	public void setTrainerFirst(String trainerFirst) {
+		this.trainerFirst = trainerFirst;
+	}
+
+	public String getTrainerLast() {
+		return trainerLast;
+	}
+
+	public void setTrainerLast(String trainerLast) {
+		this.trainerLast = trainerLast;
+	}
+
+}
