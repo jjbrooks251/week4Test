297cd66eb95ffe914435a2044acb3fe717e5b1b4
jjbrooks251 <jjbrooks251@outlook.com>
2019-06-07 10:56:18 +0100

    classroom repositories added

---
+++ src/main/java/com/qa/persistance/repository/ClassroomDateBaseRepository.java
@@ -0,0 +1,30 @@
+package com.qa.persistance.repository;
+
+import java.util.Collection;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.qa.persistance.domain.Classroom;
+import com.qa.util.JSONUtil;
+
+public class ClassroomDateBaseRepository implements ClassroomRepository {
+
+	@Inject
+	JSONUtil util;
+
+	@PersistenceContext(unitName = "primary")
+	EntityManager manager;
+
+	@Override
+	public String getAllClassrooms() {
+		Query query = manager.createQuery("SELECT a FROM Account a");
+
+		Collection<Classroom> accounts = (Collection<Classroom>) query.getResultList();
+
+		return util.getJSONForObject(accounts);
+	}
+
+}
