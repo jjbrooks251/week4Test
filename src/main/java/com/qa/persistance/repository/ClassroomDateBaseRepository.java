b030fde83c66f97cda35cbe120f277aae0ff0b6f
jjbrooks251 <jjbrooks251@outlook.com>
2019-06-07 13:45:11 +0100

    moving onto update

--- src/main/java/com/qa/persistance/repository/ClassroomDateBaseRepository.java
+++ src/main/java/com/qa/persistance/repository/ClassroomDateBaseRepository.java
@@ -46,6 +46,7 @@ public class ClassroomDateBaseRepository implements ClassroomRepository {
 		Classroom clas1 = util.getObjectForJSON(classroom, Classroom.class);
 
 		manager.persist(clas1);
+
 		return "{\"message\": \"New Account Created\"}";
 	}
 
